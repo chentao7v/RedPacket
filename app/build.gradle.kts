69cf5d90451e52c6b0769e21e846e10e963740f1
chentao <chentao7v@163.com>
2023-12-29 12:45:46 +0800

    首页布局调整；移除不必要资源

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,6 @@ android {
     targetSdk = 34
     versionCode = 100
     versionName = "1.0.0"
-
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
   signingConfigs {
@@ -63,14 +61,9 @@ android {
 }
 
 dependencies {
-
-  implementation("androidx.core:core-ktx:1.12.0")
   implementation("androidx.appcompat:appcompat:1.6.1")
   implementation("com.google.android.material:material:1.11.0")
   implementation("androidx.constraintlayout:constraintlayout:2.1.4")
   implementation("com.jakewharton.timber:timber:5.0.1")
 
-  testImplementation("junit:junit:4.13.2")
-  androidTestImplementation("androidx.test.ext:junit:1.1.5")
-  androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
