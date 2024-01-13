495bd6830fcfcf4709149cb30a15a350eead447e
chentao <chentao7v@163.com>
2024-01-13 10:32:53 +0800

    修改 min/target sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
   namespace = "me.chentao.redpacket"
-  compileSdk = 34
+  compileSdk = 33
 
   defaultConfig {
     applicationId = "me.chentao.redpacket"
     minSdk = 24
-    targetSdk = 34
+    targetSdk = 33
     versionCode = 100
     versionName = "1.0.0"
   }
@@ -68,7 +68,7 @@ android {
 
 dependencies {
   implementation("androidx.appcompat:appcompat:1.6.1")
-  implementation("com.google.android.material:material:1.11.0")
+  implementation("com.google.android.material:material:1.9.0")
   implementation("androidx.constraintlayout:constraintlayout:2.1.4")
   implementation("com.jakewharton.timber:timber:5.0.1")
   implementation("com.google.android.flexbox:flexbox:3.0.0")
