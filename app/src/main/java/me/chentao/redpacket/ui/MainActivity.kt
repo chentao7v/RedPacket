609518f73cb816b566163ac671b9fbe7f5f4bc8c
chentao <chentao7v@163.com>
2024-01-17 16:57:29 +0800

    动画调整

--- app/src/main/java/me/chentao/redpacket/ui/MainActivity.kt
+++ app/src/main/java/me/chentao/redpacket/ui/MainActivity.kt
@@ -4,6 +4,8 @@ import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
 import android.os.Bundle
 import androidx.core.view.isVisible
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import me.chentao.redpacket.R
 import me.chentao.redpacket.base.BaseActivity
 import me.chentao.redpacket.databinding.ActivityMainBinding
 import me.chentao.redpacket.service.RedPacketService
@@ -16,27 +18,52 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
   private val statusAnimator: ObjectAnimator by lazy {
     ObjectAnimator.ofFloat(binding.ivStatus, "alpha", 0f, 1f).apply {
       duration = 1000
-      repeatCount = ValueAnimator.INFINITE
+      repeatCount = 10
       repeatMode = ValueAnimator.REVERSE
     }
   }
+
+  /**
+   * 用户手动取消动画
+   */
+  private var userCancel = false
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
     binding.robot.setOnClickListener { AccessibilityTools.gotoSettingsUI(this) }
     binding.settings.setOnClickListener { SettingsActivity.launch(this) }
-    binding.ivStatus.setOnClickListener { }
+    binding.ivStatus.setOnClickListener { showRobotDialog() }
+  }
+
+  private fun showRobotDialog() {
+    statusAnimator.cancel()
+    binding.ivStatus.alpha = 1.0f
+
+    userCancel = true
+
+    MaterialAlertDialogBuilder(this)
+      .setTitle(R.string.alert_default_title)
+      .setMessage(R.string.robot_close)
+      .setPositiveButton(R.string.i_know, null)
+      .create()
+      .show()
   }
 
   private fun refreshSwitchStatus() {
     val isOpen = AccessibilityTools.isOpen(this, RedPacketService::class.java.name)
     if (isOpen) {
+      // 打开的
       binding.ivStatus.isVisible = false
-      statusAnimator.cancel()
+      if (statusAnimator.isRunning) {
+        statusAnimator.cancel()
+      }
     } else {
+      // 未打开
       binding.ivStatus.isVisible = true
-      statusAnimator.start()
+      if (!userCancel) {
+        statusAnimator.start()
+      }
     }
   }
 
@@ -47,7 +74,9 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
 
   override fun onStop() {
     super.onStop()
-    statusAnimator.cancel()
+    if (statusAnimator.isRunning) {
+      statusAnimator.cancel()
+    }
   }
 
 }
