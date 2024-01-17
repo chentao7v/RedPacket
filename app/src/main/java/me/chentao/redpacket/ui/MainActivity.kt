de907a009b3b2ff84ee05265f1a29c447460d1de
chentao <chentao7v@163.com>
2024-01-17 17:51:13 +0800

    动画封装

--- app/src/main/java/me/chentao/redpacket/ui/MainActivity.kt
+++ app/src/main/java/me/chentao/redpacket/ui/MainActivity.kt
@@ -1,32 +1,19 @@
 package me.chentao.redpacket.ui
 
-import android.animation.ObjectAnimator
-import android.animation.ValueAnimator
 import android.os.Bundle
-import androidx.core.view.isVisible
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import me.chentao.redpacket.R
 import me.chentao.redpacket.base.BaseActivity
 import me.chentao.redpacket.databinding.ActivityMainBinding
 import me.chentao.redpacket.service.RedPacketService
 import me.chentao.redpacket.utils.AccessibilityTools
+import me.chentao.redpacket.utils.StatusAlphaAnimator
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
   override fun getViewBinding() = ActivityMainBinding.inflate(layoutInflater)
 
-  private val statusAnimator: ObjectAnimator by lazy {
-    ObjectAnimator.ofFloat(binding.ivStatus, "alpha", 0f, 1f).apply {
-      duration = 1000
-      repeatCount = 10
-      repeatMode = ValueAnimator.REVERSE
-    }
-  }
-
-  /**
-   * 用户手动取消动画
-   */
-  private var userCancel = false
+  private lateinit var statusAnimator: StatusAlphaAnimator
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -34,13 +21,12 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     binding.robot.setOnClickListener { AccessibilityTools.gotoSettingsUI(this) }
     binding.settings.setOnClickListener { SettingsActivity.launch(this) }
     binding.ivStatus.setOnClickListener { showRobotDialog() }
+
+    statusAnimator = StatusAlphaAnimator(binding.ivStatus)
   }
 
   private fun showRobotDialog() {
-    statusAnimator.cancel()
-    binding.ivStatus.alpha = 1.0f
-
-    userCancel = true
+    statusAnimator.cancelManual()
 
     MaterialAlertDialogBuilder(this)
       .setTitle(R.string.alert_default_title)
@@ -50,33 +36,15 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
       .show()
   }
 
-  private fun refreshSwitchStatus() {
-    val isOpen = AccessibilityTools.isOpen(this, RedPacketService::class.java.name)
-    if (isOpen) {
-      // 打开的
-      binding.ivStatus.isVisible = false
-      if (statusAnimator.isRunning) {
-        statusAnimator.cancel()
-      }
-    } else {
-      // 未打开
-      binding.ivStatus.isVisible = true
-      if (!userCancel) {
-        statusAnimator.start()
-      }
-    }
-  }
-
   override fun onResume() {
     super.onResume()
-    refreshSwitchStatus()
+    statusAnimator.refreshStatus(AccessibilityTools.isOpen(this, RedPacketService::class.java.name))
   }
 
   override fun onStop() {
     super.onStop()
-    if (statusAnimator.isRunning) {
-      statusAnimator.cancel()
-    }
+
+    statusAnimator.cancel()
   }
 
 }
---
+++ app/src/main/java/me/chentao/redpacket/utils/StatusAlphaAnimator.kt
@@ -0,0 +1,54 @@
+package me.chentao.redpacket.utils
+
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.view.View
+import androidx.core.view.isVisible
+
+/**
+ * create by chentao on 2024-01-17.
+ */
+class StatusAlphaAnimator(private val ivStatus: View) {
+
+  private val statusAnimator: ObjectAnimator by lazy {
+    ObjectAnimator.ofFloat(ivStatus, "alpha", 0f, 1f).apply {
+      duration = 1000
+      repeatCount = 10
+      repeatMode = ValueAnimator.REVERSE
+    }
+  }
+
+  /**
+   * 用户手动取消动画
+   */
+  private var userCancel = false
+
+  fun cancelManual() {
+    userCancel = true
+    statusAnimator.cancel()
+    ivStatus.alpha = 1.0f
+  }
+
+  fun refreshStatus(isOpen: Boolean) {
+    if (isOpen) {
+      // 打开的
+      ivStatus.isVisible = false
+      if (statusAnimator.isRunning) {
+        statusAnimator.cancel()
+      }
+    } else {
+      // 未打开
+      ivStatus.isVisible = true
+      if (!userCancel) {
+        statusAnimator.start()
+      }
+    }
+  }
+
+  fun cancel() {
+    if (statusAnimator.isRunning) {
+      statusAnimator.cancel()
+    }
+  }
+
+}
